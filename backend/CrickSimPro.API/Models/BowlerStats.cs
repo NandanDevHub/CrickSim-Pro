1d9dfbd4f16745cbdd528a78ec730db1bc517481
nandan <parmar.nandan189@gmail.com>
2025-08-05 13:38:32 +0530

    Refactor BatterStats and BowlerStats models to enforce required properties for Name and Type

--- backend/CrickSimPro.API/Models/BowlerStats.cs
+++ backend/CrickSimPro.API/Models/BowlerStats.cs
@@ -2,12 +2,11 @@ namespace CrickSimPro.API.Models
 {
     public class BowlerStats
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public double Overs { get; set; }
         public int BallsBowled { get; set; }
         public int RunsConceded { get; set; }
         public int Wickets { get; set; }
-        public double Economy => BallsBowled > 0 ? (RunsConceded * 6.0) / BallsBowled : 0;
-        // TO Do Next add: Maidens, NoBalls, Wides, etc.
+        public List<string> WicketModes { get; set; } = [];
     }
 }
