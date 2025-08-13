53b34273ebf6965c3162028dcfb6672f46a4b10d
nandan <parmar.nandan189@gmail.com>
2025-08-13 10:22:43 +0530

    Make Message, Pitch, and Weather properties required in SimulationResult model

--- backend/CrickSimPro.API/Models/SimulationResult.cs
+++ backend/CrickSimPro.API/Models/SimulationResult.cs
@@ -2,16 +2,16 @@ namespace CrickSimPro.API.Models
 {
     public class SimulationResult
     {
-        public string Message { get; set; }
-        public string Pitch { get; set; }
-        public string Weather { get; set; }
+        public required string Message { get; set; }
+        public required string Pitch { get; set; }
+        public required string Weather { get; set; }
         public int Runs { get; set; }
         public int Wickets { get; set; }
-        public List<List<string>> OversDetail { get; set; }
-        public List<OverStat> OverStats { get; set; }
-        public List<BatterStats> BatterStats { get; set; }
-        public List<BowlerStats> BowlerStats { get; set; }
-        public bool IsChase { get; set; }         // true if this is a chase/second innings
-        public int? TargetScore { get; set; }     // target, if chasing
+        public List<List<string>>? OversDetail { get; set; }
+        public List<OverStat>? OverStats { get; set; }
+        public List<BatterStats>? BatterStats { get; set; }
+        public List<BowlerStats>? BowlerStats { get; set; }
+        public bool IsChase { get; set; }
+        public int? TargetScore { get; set; }
     }
 }
